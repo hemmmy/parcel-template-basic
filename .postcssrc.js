b5b5d4dbefc373c2b4c3e373a17405c9058e82fe
hyemin <hyemin52@naver.com>
2021-11-18 11:46:30 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,18 @@
+// ESM
+// Common JS
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+/* 
+export {
+    plugins : [
+        autoprefixer
+    ]
+}
+*/ 
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
